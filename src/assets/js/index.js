74b85d8cbd680d0c021da1c0f0ccd8f86a14e734
aoslovcan <tonkica1605.o@gmail.com>
2024-11-19 16:39:53 +0100

    fix the test image import

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,51 +1,20 @@
 import '../scss/styles.scss';
-import {gsap} from "gsap";
-
-import textImage from '../images/text-image.svg';
-import recipe1 from '../images/recipe-1.svg';
-import recipe2 from "../images/recipe-2.svg";
-import recipe3 from "../images/recipe-3.svg";
-import graphic1 from '../images/graphic-1.svg';
-import graphic2 from "../images/graphic-2.svg";
-import graphic3 from "../images/graphic-3.png";
-import graphic4 from "../images/graphic-4.svg";
-import person1 from "../images/man.jpg";
-import person2 from "../images/person-2.svg";
-
-
 import Slider from "./components/Slider";
 import {TextImageSection} from "./components/TextImageSection"
 import NavigationHandler from "./components/NavigationHandler";
 import MobileNavigationHandler from "./components/MobileNavigationHandler";
 import CursorController from "./components/CursorController";
 
-// Dynamically update the `src` of the image in your HTML
-const imgElement = document.querySelector('.c-text-image-section img');
-imgElement.src = textImage;
-
-const recipe1Img = document.querySelector('.js-image-1')
-const recipe2Img = document.querySelector('.js-image-2')
-const recipe3Img = document.querySelector('.js-image-3')
-const firstGraphic = document.querySelector('.js-graphic-1 ')
-const secondGraphic = document.querySelector('.js-graphic-2 ')
-const thirdGraphic = document.querySelector('.js-graphic-3 ')
-const fourthGraphic = document.querySelector('.js-graphic-4 ')
-
-const personImg1 = document.querySelectorAll(".js-person-1")
-const personImg2 = document.querySelector(".js-person-2")
-
-for(let i = 0; i < personImg1.length; i++){
-    personImg1[i].src = person1
-}
-
-recipe1Img.src = recipe1
-recipe2Img.src = recipe2
-recipe3Img.src = recipe3
-firstGraphic.src = graphic1
-secondGraphic.src = graphic2
-thirdGraphic.src = graphic3
-fourthGraphic.src = graphic4
-personImg2.src = person2
+import textImage from '../images/text-image.svg';
+import recipe1 from '../images/recipe-1.svg';
+import recipe2 from "../images/recipe-2.svg";
+import recipe3 from "../images/recipe-3.svg";
+import graphic1 from "../images/graphic-1.jpg";
+import graphic2 from "../images/graphic-2.svg";
+import graphic3 from "../images/graphic-3.png";
+import graphic4 from "../images/graphic-4.svg";
+import person1 from "../images/man.jpg";
+import person2 from "../images/person-2.svg";
 
 const ready = (callback) => {
     if (document.readyState !== "loading") {
